6067ead5df2e3e715fbf3baa6fc6f6713a0017d6
Mario Rodas <marsam@users.noreply.github.com>
2016-10-14 10:20:30 -0500

    Drop support for Emacs<=25
    Currently cl-generic from gnu elpa doesn't support specializers.

--- python-test.el
+++ python-test.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/emacs-pe/python-test.el
 ;; Keywords: convenience, tools, processes
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.3") (cl-generic "0.3"))
+;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
